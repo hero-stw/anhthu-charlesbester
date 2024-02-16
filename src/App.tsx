b3e88149a9858e65232509b49956bf031b769f51
hungpa1 <hungpa1@fpt.com>
2024-02-16 20:50:13 +0700

    FEAT: Change font size

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,24 @@
-import {Route, Routes} from "react-router-dom";
+import {Outlet, Route, Routes} from "react-router-dom";
 import Home from "./pages/Home.tsx";
 import RSVPList from "./pages/RSVPList.tsx";
 
 function App() {
   return (
     <Routes>
-      <Route path={"/"} element={<Home/>}/>
-      <Route path={"rsvp"} element={<RSVPList/>}/>
+        <Route path="/" element={<Layout />}>
+            <Route index element={<Home />} />
+            <Route path="about" element={<RSVPList />} />
+        </Route>
     </Routes>
   );
 }
 
 export default App;
+
+function Layout() {
+    return (
+        <div>
+            <Outlet />
+        </div>
+    );
+}
